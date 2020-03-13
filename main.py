667f730ac9e822f722355f4e6b10c9792defe258
TobiasTR <tobiast98kontakt@protonmail.com>
2020-03-13 18:34:00 +0100

    sender med passord og headers lik appen

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 import requests
+import json
+import socket
+import sys
+import time
 
-url = '192.168.2.72:8123'
+if len(sys.argv) != 2:
+    print("Missing url to hass os")
+    sys.exit(1)
 
-params = {'username': 'RandomDude', 'password': '824rhewjf3'}
+url = str(sys.argv[1])
 
-x = requests.post(url, data = myobj)
+data = {'username': 'admin', 
+        'password': 'admin',
+        'client_id': 'https://home-assistant.io/android',
+        }
+
+
+headers = {
+"Host": "10.0.0.137:8123",
+"Connection": "keep-alive",
+"Content-Length": "87",
+"Origin": "http://10.0.0.137:8123",
+"User-Agent": "Mozilla/5.0 (Linux; Android 9; Android SDK built for x86 Build/PSR1.180720.093; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/69.0.3497.100 Mobile Safari/537.36",
+"Content-Type": "text/plain;charset=UTF-8",
+"Accept": "*/*",
+"Referer": "http://10.0.0.137:8123/auth/authorize?response_type=code&client_id=https://home-assistant.io/android&redirect_uri=homeassistant://auth-callback",
+"Accept-Encoding": "gzip, deflate",
+"Accept-Language": "en-US",
+"X-Requested-With": "io.homeassistant.companion.android.debug"
+}
+
+
+post = requests.post(url, data=json.dumps(data), headers=headers)
+print(post.status_code)
